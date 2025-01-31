e16367fec291fe376a918dc538f66cde11b0e3e6
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2025-01-31 13:50:22 +0530

    Fix CI by adjusting Next configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,14 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
     pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+
+    // GitHub pages deployment configuration:
+    output: "export",
+    basePath: "/",
+    images: {
+        unoptimized: true,
+    },
 };
 
 const withMDX = createMDX();
